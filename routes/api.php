da4d67c03972f7410722ead48da959a3784216aa
abela-a <abelardhana96@gmail.com>
2023-12-31 22:43:24 +0800

    install dingo/api dan init api v1

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,5 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,6 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::get('/', function () {
+    echo 'REST API for Mind Box App';
 });
+
+$files = glob(__DIR__.'/api/*.php');
+
+foreach ($files as $file) {
+    require_once $file;
+}
---
+++ routes/api/v1.php
@@ -0,0 +1,10 @@
+<?php
+
+use Dingo\Api\Routing\Router;
+
+/** @var \Dingo\Api\Routing\Router $api */
+$api = app('Dingo\Api\Routing\Router');
+
+$api->version('v1', function (Router $api) {
+
+});
